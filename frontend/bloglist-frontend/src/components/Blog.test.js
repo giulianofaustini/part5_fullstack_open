36d3e4bbcd322eab99960b4aabf47d1c33360b43
Giuliano Faustini <giuliperus@gmail.com>
2023-10-04 16:20:05 +0300

    Added default 0 for like

--- frontend/bloglist-frontend/src/components/Blog.test.js
+++ frontend/bloglist-frontend/src/components/Blog.test.js
@@ -8,12 +8,15 @@ test('renders title and author, but not URL or likes by default', () => {
   const blog = {
     title: 'Test Blog Title',
     author: 'Test Author',
+    likes: 0,
   }
 
   render(<Blog blog={blog} />)
 
   expect(screen.getByText(/Title: Test Blog Title/)).toBeInTheDocument()
   expect(screen.getByText(/Author: Test Author/)).toBeInTheDocument()
+
+  expect(screen.getByText(/Likes: 0/)).toBeInTheDocument()
 
   expect(screen.queryByText(/URL:/)).not.toBeInTheDocument()
 })
